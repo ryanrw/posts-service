1a6447cf7970c13517d8ad538e31e32b25a2fc2e
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-19 02:01:49 +0700

    [0pdd] add todo

--- src/graphql/typedefs/addPost.ts
+++ src/graphql/typedefs/addPost.ts
@@ -1,7 +1,7 @@
 // example query
 import { gql } from 'apollo-server'
 
-// @todo add post query
+// @todo #1 add post query
 export default gql`
   extend type Mutation {
     addPost(title: String!, content: String!): Status!
